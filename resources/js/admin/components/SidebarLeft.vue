c21b3d9c7dccd4fe30d7a67945c8ec917ea9fe58
WEB-ESPN <141797850+WEB-ESPN@users.noreply.github.com>
2023-11-23 10:53:58 +0700

    Update171123 API (#5)
    * upadate admin
    
    * update add data
    
    * Update vite.config.js
    
    * finnish add
    
    * add api
    
    * Update new API
    
    ---------
    
    Co-authored-by: web-espn <elisabeth.christin@student.ukdc.ac.id>
    Co-authored-by: web-espn <web-espn@elevenia.co.id>

--- resources/js/admin/components/SidebarLeft.vue
+++ resources/js/admin/components/SidebarLeft.vue
@@ -2,16 +2,16 @@
     <aside class="main-sidebar sidebar-dark-primary elevation-4">
 
         <a href="index3.html" class="brand-link">
-            <img src="/assets/images/nusantara.png" alt="AdminLTE Logo"
-                style="heigh: 10px" >
-            <!-- <span class="brand-text font-weight-light">ENUSA</span> -->
+            <img src="https://adminlte.io/themes/v3/dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3"
+                style="opacity: .8">
+            <span class="brand-text font-weight-light">ENUSA</span>
         </a>
 
         <div class="sidebar">
 
             <div class="user-panel mt-3 pb-3 mb-3 d-flex">
                 <div class="image">
-                    <img src="/assets/images/logo admin enusa.png" class="img-circle elevation-3" alt="User Image" style="height:30px">
+                    <img src="https://adminlte.io/themes/v3/dist/img/AdminLTELogo.png" class="img-circle elevation-2" alt="User Image">
                 </div>
                 <div class="info">
                     <router-link to="/admin/profile" class="d-block">{{ authStore.user.email }}</router-link>
