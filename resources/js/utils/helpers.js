c1c0fceda2f8f923d0d8d4ea80c00de78adfe174
web-espn <web-espn@elevenia.co.id>
2023-11-24 07:55:03 +0700

    delete file

--- resources/js/utils/helpers.js
+++
@@ -1,4 +0,0 @@
-export const titleCase = (str) => { 
-    return str.replace (/^[-_]*(.)/, (_, c) => c.toUpperCase()) // Initial char (after -/_)
-        .replace (/[-_]+(.)/g, (_, c) => ' ' + c.toUpperCase()) // First char after each -/_
-}
